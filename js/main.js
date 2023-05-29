7c94e7269d953ab9a29a05705df98f75a0873793
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-05-29 17:53:41 +0200

    fine lavoro + bonus

--- js/main.js
+++ js/main.js
@@ -7,15 +7,19 @@ createApp({
         };
     },
 
-    mounted () {
-        axios
-            .get (`https://flynn.boolean.careers/exercises/api/random/mail`)
-            .then((result) => {
-                let nuovaMail = result.data.response;
-                this.mailList.push(nuovaMail);
-                console.log(this.mailList);
-            });
-            
+    methods: {
+        stampaMails() {
+            for (let i = 0; i < 10; i++) {
+                axios
+                    .get (`https://flynn.boolean.careers/exercises/api/random/mail`)
+                    .then((result) => {
+                        let nuovaMail = result.data.response;
+                        this.mailList.push(nuovaMail);
+                    });
+            };
+        }
     }
+
+    
 
 }).mount("#app");
